ca08748b5b5ff127f81eb42283836b2d6fb27f07
a lan <bluenowte97@gmail.com>
2018-11-06 23:13:13 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def long_planeteer_calls(words)# code an argument here
   # Your code here
   words.any? do |word|
     word.length > 4
-  end  
+  end
 end
 
 def find_the_cheese# code an argument here
